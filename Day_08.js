d31ac536eb6c4d2aa35f7ab1805054eb21e30b59
Anandprabhu530 <97842562+Anandprabhu530@users.noreply.github.com>
2023-05-12 19:22:31 +0530

    Added Day_08.js

---
+++ Day_08.js
@@ -0,0 +1,31 @@
+//*
+
+2666. Allow One Function Call
+
+https://leetcode.com/problems/allow-one-function-call/description/?gio_link_id=a9By01Oo
+
+My-Submission: https://leetcode.com/problems/allow-one-function-call/solutions/3516347/easy-js-solution/
+
+*/
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    let is_called = true
+    return function(...args){
+        if(is_called){
+            is_called = false
+            return fn(...args)
+        }
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
